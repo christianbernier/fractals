cf1b481fc87e9f893bebaf440bba7195d58d22b3
Plasmabot1 <sawhney.anhad@gmail.com>
2019-04-11 19:25:44 -0400

    Create ComputeHandler.java

---
+++ com/fractal/compute/ComputeHandler.java
@@ -0,0 +1,20 @@
+package com.fractal.compute;
+
+import org.lwjgl.opencl.Util;
+import org.lwjgl.opencl.CLMem;
+import org.lwjgl.opencl.CLCommandQueue;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.opencl.CLProgram;
+import org.lwjgl.opencl.CLKernel;
+import java.nio.FloatBuffer;
+import java.util.List;
+import org.lwjgl.opencl.CL;
+import org.lwjgl.opencl.CLContext;
+import org.lwjgl.opencl.CLDevice;
+import org.lwjgl.opencl.CLPlatform;
+import static org.lwjgl.opencl.CL10.*;
+
+public class ComputeHandler {
+
+}
