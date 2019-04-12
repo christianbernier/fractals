5e78b4ecfbfb3026ec5367c84814f27adba6f1d8
christianbernier <35120006+christianbernier@users.noreply.github.com>
2019-04-12 13:11:56 -0400

    Added mouse functionality

--- com/fractal/graphics/input/MouseInput.java
+++ com/fractal/graphics/input/MouseInput.java
@@ -4,9 +4,18 @@ import org.lwjgl.glfw.GLFWCursorPosCallback;
 
 public class MouseInput extends GLFWCursorPosCallback{
 
+	int w, h;
+	
+	public MouseInput(int w, int h) {
+		this.w = w;
+		this.h = h;
+	}
+	
 	@Override
 	public void invoke(long window, double xpos, double ypos) {
-		System.out.println("Mouse x: " + xpos + " y: " + ypos);
+		double x = xpos - (w / 2);
+		double y = ypos - (h / 2);
+		System.out.println("Mouse [x: " + x + " y: " + y + "]");
 	}
 
 }
