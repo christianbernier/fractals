4321cc7705b648a5132bc502f1d56945eb0fd51b
christianbernier <35120006+christianbernier@users.noreply.github.com>
2019-04-12 13:11:30 -0400

    Added mouse functionality

--- com/fractal/graphics/GUIWindow.java
+++ com/fractal/graphics/GUIWindow.java
@@ -8,7 +8,6 @@ import org.lwjgl.glfw.GLFWCursorPosCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 
-
 import com.fractal.graphics.input.KeyboardInput;
 import com.fractal.graphics.input.MouseInput;
 import com.fractal.graphics.fractalEngine.PixelObject;
@@ -17,11 +16,11 @@ public class GUIWindow{
 	
 	public boolean running = false;
 	public long window;
-	public int width = 800;
-	public int height = 600;
+	public static int width = 800;
+	public static int height = 600;
 	
 	private static final GLFWKeyCallback keyCallback = new KeyboardInput();
-	private static final GLFWCursorPosCallback cursorCallback = new MouseInput();
+	private static final GLFWCursorPosCallback cursorCallback = new MouseInput(width, height);
 	
 	PixelObject pixels;
 	
@@ -40,6 +39,7 @@ public class GUIWindow{
 			System.err.println("Could not create Window");
 		}
 		
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 		glfwSetKeyCallback(window, keyCallback);
 		glfwSetCursorPosCallback(window, cursorCallback);
 		
@@ -54,11 +54,11 @@ public class GUIWindow{
 		
 		
 		int[] x = new int[] {
-			-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5	
+			-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
 		};
 		
 		int[] y = new int[] {
-			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0	
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 		};
 		
 		float[] r = new float[] {
@@ -66,12 +66,15 @@ public class GUIWindow{
 		};
 		
 		float[] g = new float[] {
-			0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1f	
+			0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1f
 		};
 		
 		float[] b = new float[] {
-			0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1f	
+			0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1f
 		};
+		
+		
+		
 		pixels = new PixelObject(1, width, height, x, y, r, g, b);
 	}
 	
